6653385e09aea32a4aa207df2860325b99a57a67
Sainath Shinde <shindesainath692@gmail.com>
2025-03-12 17:11:43 +0530

    Home page design and navigation tab added

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Link } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-10 mb-5 mx-auto" />
+      </ScrollView>
+    </View>
+  );
+}
